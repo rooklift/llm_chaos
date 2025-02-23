a2500c81dc22a2009d29267c3706955ae0b1be63
rooklift <16438795+rooklift@users.noreply.github.com>
2025-02-23 12:41:01 +0000

    Update configs.js

--- ai/configs.js
+++ ai/configs.js
@@ -4,7 +4,8 @@
 //
 //	max_tokens_key		-- The key used in the data object when specifying the max tokens value; OpenAI deprecated "max_tokens"
 //	sp_role				-- The role for IN-ARRAY system prompt messages, typically "system" but maybe "developer" or "user"
-//	temperature			-- Likely 0-1, but use -1 if we shouldn't send this field
+//
+//  show_reasoning		-- Purely internal flag for OpenRouter requests - we should always receive the reasoning, but do we embed it in the response?
 //
 // Note on OpenRouter reasoning models:
 // For OpenRouter, we have to set "include_reasoning" if we want to get sent the reasoning (if any).
@@ -17,18 +18,18 @@ exports.Required = ["model", "name", "company", "url"];
 
 exports.Defaults = {
 
+	version: "",					// Like "name", this is mostly for system prompts, etc (not needed for comms).
+
 	max_tokens_key: "max_tokens",
 	sp_role: "system",
-
-	version: "",					// Like "name", this is mostly for system prompts, etc (not needed for comms).
 
 	max_tokens: 8192,
 	system_prompt: "",				// Not sent if ""
 	temperature: -1,				// Not sent if -1
 	reasoning_effort: "",			// Not sent if ""			(OpenAI API)
-	anthropic_version: "",			// Not sent if ""
+	anthropic_version: "",			// Not sent if ""			(Irrelevant for non-Anthropic LLMs)
 
-	show_reasoning: false,			// Internal flag for OpenRouter requests - we should always receive the reasoning, but do we display it?
+	show_reasoning: false,
 	max_errors: 10,
 	min_delay: 1,
 	max_delay: 16,
