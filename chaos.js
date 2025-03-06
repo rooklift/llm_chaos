831d4855ebdb8cc8f7a91548b4affc2270cc0539
rooklift <16438795+rooklift@users.noreply.github.com>
2025-03-06 10:17:29 +0000

    Oops || is the wrong thing

--- chaos.js
+++ chaos.js
@@ -105,21 +105,21 @@ const bot_prototype = {
 				ai_client: null,														// Connection to the LLM via the ai library.
 				conn: null,																// Connection to Discord.
 
-				top_header: cfg.top_header || common.top_header || "",					// System header to include at start of a foreign message block.
-				end_header: cfg.end_header || common.end_header || "",					// System header to include at end of a foreign message block.
+				top_header: cfg.top_header ?? common.top_header ?? "",					// System header to include at start of a foreign message block.
+				end_header: cfg.end_header ?? common.end_header ?? "",					// System header to include at end of a foreign message block.
 
 				owner: common.owner,													// Name of the human in charge...
-				owner_id: common.owner_id || "",										// And their Discord ID as a string
-				chaos: cfg.chaos || common.chaos || 0,									// Chance of responding randomly to a non-ping.
-				emoji: cfg.emoji || common.emoji || "💡",								// Emoji used to acknowledge receipt of message.
-				sp_location: cfg.system_prompt || common.system_prompt || "",			// Location of the system prompt, for (re)loading.
-				ping_blind: cfg.ping_blind || common.ping_blind || false,				// Whether this LLM's ping recognition is suppressed.
-				show_reasoning: cfg.show_reasoning || common.show_reasoning || true,	// Whether thinking blocks are shown (if available).
-				history_limit: cfg.history_limit || common.history_limit || 50,			// Max history length.
-				poll_wait: cfg.poll_wait || common.poll_wait || 20000,					// Delay for maybe_respond_spinner().
-				restricted: cfg.restricted || false,									// Only the owner can cause a reply.
-				input_price: cfg.input_price || 0,										// Expressed as dollars per million tokens.
-				output_price: cfg.output_price || 0,									// Note that there are issues with not counting reasoning tokens.
+				owner_id: common.owner_id ?? "",										// And their Discord ID as a string
+				chaos: cfg.chaos ?? common.chaos ?? 0,									// Chance of responding randomly to a non-ping.
+				emoji: cfg.emoji ?? common.emoji ?? "💡",								// Emoji used to acknowledge receipt of message.
+				sp_location: cfg.system_prompt ?? common.system_prompt ?? "",			// Location of the system prompt, for (re)loading.
+				ping_blind: cfg.ping_blind ?? common.ping_blind ?? false,				// Whether this LLM's ping recognition is suppressed.
+				show_reasoning: cfg.show_reasoning ?? common.show_reasoning ?? true,	// Whether thinking blocks are shown (if available).
+				history_limit: cfg.history_limit ?? common.history_limit ?? 50,			// Max history length.
+				poll_wait: cfg.poll_wait ?? common.poll_wait ?? 20000,					// Delay for maybe_respond_spinner().
+				restricted: cfg.restricted ?? false,									// Only the owner can cause a reply.
+				input_price: cfg.input_price ?? 0,										// Expressed as dollars per million tokens.
+				output_price: cfg.output_price ?? 0,									// Note that there are issues with not counting reasoning tokens.
 
 				sent_tokens: 0,															// Count, usually based on actual metadata in response JSON.
 				received_tokens: 0,														// Likewise.
