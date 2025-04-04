44c56e9c4e00d046a549e2cf7a93e439aef52d69
rooklift <16438795+rooklift@users.noreply.github.com>
2025-04-04 22:56:10 +0100

    No need for author IDs now

--- chaos.js
+++ chaos.js
@@ -61,11 +61,11 @@ const history_object_prototype = {
 };
 
 function normal_system_header(o) {
-	return `### Message from ${o.author_name} (${o.author_type}, userid ${o.author_id}):\n`;
+	return `### Message from ${o.author_name} (${o.author_type}):\n`;
 }
 
 function attachment_system_header(o) {
-	return `### File attached by ${o.author_name} (${o.author_type}, userid ${o.author_id}):\n`;
+	return `### File attached by ${o.author_name} (${o.author_type}):\n`;
 }
 
 // ------------------------------------------------------------------------------------------------
